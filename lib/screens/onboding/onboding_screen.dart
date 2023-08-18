58cf682771e3b9f2924dc02d152e9afe4515af71
DELL <anjugupta7060@gmail.com>
2023-08-18 21:08:23 +0530

    Onboarding Blur Animation

--- lib/screens/onboding/onboding_screen.dart
+++ lib/screens/onboding/onboding_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -10,6 +13,28 @@ class OnboardingScreen extends StatefulWidget {
 class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            width: MediaQuery.of(context).size.width *1.7,
+            bottom: 200,
+            left: 100,
+            child: Image.asset("assets/Backgrounds/Spline.png")),
+            Positioned.fill(child: BackdropFilter(filter: ImageFilter.blur(
+            sigmaX: 20,
+            sigmaY: 10
+          ),child: SizedBox(),),),
+        RiveAnimation.asset("assets/RiveAssets/shapes.riv",fit: BoxFit.cover,),
+
+          Positioned.fill(child: BackdropFilter(filter: ImageFilter.blur(
+            sigmaX: 50,
+            sigmaY: 30
+          ),child: SizedBox(),),),
+
+         
+        ],
+      
+    ),);
   }
 }
