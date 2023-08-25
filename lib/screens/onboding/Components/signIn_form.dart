7b2c81c9f3d129b72ffa2f8294bed3539e1bd50b
DELL <anjugupta7060@gmail.com>
2023-08-25 19:32:43 +0530

    SignIn Validation

--- lib/screens/onboding/Components/signIn_form.dart
+++ lib/screens/onboding/Components/signIn_form.dart
@@ -1,66 +1,168 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:rive/rive.dart';
 
-
-
-
-class SignInForm extends StatelessWidget {
+class SignInForm extends StatefulWidget {
   const SignInForm({
     super.key,
   });
 
   @override
+  State<SignInForm> createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late SMITrigger check;
+  late SMITrigger error;
+  late SMITrigger reset;
+
+  bool isShowLoading = false;
+
+  StateMachineController getRiveController(Artboard artboard) {
+    StateMachineController? controller =
+        StateMachineController.fromArtboard(artboard, "state Machine 1");
+    artboard.addController(controller!);
+    return controller;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Form(child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Stack(
       children: [
-      Text("Email",style: TextStyle(
-        color: Colors.black54
-      ),),
-      Padding(
-        padding: const EdgeInsets.only(top:8.0,bottom: 16),
-        child: TextFormField(
-          decoration: InputDecoration(
-            prefixIcon: Padding(
-              padding:  EdgeInsets.symmetric(horizontal: 8),
-              child: SvgPicture.asset("assets/icons/email.svg"),
-            )
+        Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Email",
+                style: TextStyle(color: Colors.black54),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 16),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isNotEmpty) {
+                      return "";
+                    }
+                    return null;
+                  },
+                  onSaved: (email) {},
+                  decoration: InputDecoration(
+                      prefixIcon: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: SvgPicture.asset("assets/icons/email.svg"),
+                  )),
+                ),
+              ),
+              Text(
+                "Password",
+                style: TextStyle(color: Colors.black54),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 16),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value!.isNotEmpty) {
+                      return "";
+                    }
+                    return null;
+                  },
+                  onSaved: (password) {},
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      prefixIcon: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    child: SvgPicture.asset("assets/icons/password.svg"),
+                  )),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 24),
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFF77D8E),
+                    minimumSize: Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(25),
+                      bottomLeft: Radius.circular(25),
+                      bottomRight: Radius.circular(25),
+                    )),
+                  ),
+                  onPressed: () {
+                    //show the loading animation when button is clicked
+                    setState(() {
+                      isShowLoading = true;
+                    });
+                    Future.delayed(Duration(seconds: 1), () {
+                      if (_formKey.currentState!.validate()) {
+                        //success
+                        check.fire();
+                        Future.delayed(Duration(seconds: 2), () {
+                          setState(() {
+                            isShowLoading = false;
+                          });
+                        });
+                      } else {
+                        //error
+                        error.fire();
+                        Future.delayed(Duration(seconds: 2), () {
+                          setState(() {
+                            isShowLoading = false;
+                          });
+                        });
+                      }
+                    });
+                  },
+                  icon: Icon(
+                    Icons.arrow_forward,
+                    color: Color(0xFFFE0037),
+                  ),
+                  label: Text("Sign In"),
+                ),
+              ),
+            ],
           ),
         ),
-      ),
-      Text("Password",style: TextStyle(
-        color: Colors.black54
-      ),),
-      Padding(
-        padding: const EdgeInsets.only(top:8.0,bottom: 16),
-        child: TextFormField(
-          obscureText: true,
-          decoration: InputDecoration(
-            prefixIcon: Padding(
-              padding:  EdgeInsets.symmetric(horizontal: 8),
-              child: SvgPicture.asset("assets/icons/password.svg"),
-            )
-          ),
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.only(top:8.0,bottom: 24),
-        child: ElevatedButton.icon(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Color(0xFFF77D8E),
-            minimumSize: Size(double.infinity, 50),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(25),
-                bottomLeft: Radius.circular(25),
-                bottomRight: Radius.circular(25),
+        isShowLoading
+            ? CustomPositioned(
+                child: RiveAnimation.asset(
+                  "assets/RiveAssets/check.riv",
+                  onInit: (artboard) {
+                    StateMachineController controller =
+                        getRiveController(artboard);
+                    check = controller.findSMI("Check") as SMITrigger;
+                    error = controller.findSMI("Error") as SMITrigger;
+                    reset = controller.findSMI("Reset") as SMITrigger;
+                  },
+                ),
               )
-            ),
+            : SizedBox(),
+      ],
+    );
+  }
+}
+
+class CustomPositioned extends StatelessWidget {
+  const CustomPositioned({super.key, required this.child, this.size = 100});
+
+  final Widget child;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: Column(
+        children: [
+          Spacer(),
+          SizedBox(height: 100, width: 100, child: child),
+          Spacer(
+            flex: 2,
           ),
-          onPressed: (){}, icon: Icon(Icons.arrow_forward,
-          color: Color(0xFFFE0037),), label: Text("Sign In"),),
+        ],
       ),
-    ],),);
+    );
   }
 }
